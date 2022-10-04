4d1efb27ef9330e0eb4b9ce2959fcc6f8e9c42d3
jalilpablo <jalilpablo@gmail.com>
2022-10-04 14:47:33 -0300

    validation_function

---
+++ data_validation.py
@@ -0,0 +1,34 @@
+
+#data validation function
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No data loaded")
+        return False 
+
+    # Primary Key Check
+    if pd.Series(df['PRIMARY_KEY_COLUMN']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key check is violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    #you can add any validation you want
+
+    return True
+
+
+
+if __name__ == "__main__":
+
+    # EXTRACCION PROCESS
+
+    # Validate
+    if check_if_valid_data(song_df):
+        print("Data valid, proceed to Load stage")
+
+
+    #LOAD STAGE
